c477eb7109ed62161ab4444e8760345687f2859e
Rodrigo Marques <rodrigomarques1993@gmail.com>
2015-11-04 21:26:10 -0200

    Added TaggedAutomataReader and WriteTaggedAutomata
    Added TaggedAutomataReader and WriteTaggedAutomata.
    Starting to implement TaggedAutomataReader

---
+++ src/main/lexer/model/commandline/WriteTaggedAutomata.java
@@ -0,0 +1,17 @@
+/**
+ * UNIVERSIDADE FEDERAL DE SANTA CATARINA
+ * INE - DEPARTAMENTO DE INFORMÁTICA E ESTATÍSTICA
+ * LINGUAGENS FORMAIS E COMPILADORES
+ * @author LUCAS FINGER ROMAN
+ * @author RODRIGO PEDRO MARQUES
+ * Copyright © 2015
+ */
+package main.lexer.model.commandline;
+
+/**
+ * @author Rodrigo
+ *
+ */
+class WriteTaggedAutomata {
+
+}
