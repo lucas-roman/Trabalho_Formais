32e925bf49aea11d606fda4d7fb61f38efe9b1ee
lucas-roman <lu_roman@hotmail.com>
2015-11-23 20:56:05 -0200

    update

--- src/main/parser/grammar/ContextFreeGrammar.java
+++ src/main/parser/grammar/ContextFreeGrammar.java
@@ -1,7 +1,23 @@
 package main.parser.grammar;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContextFreeGrammar {
-
-
 	
+	private Map<String, ContextFreeTerminalSymbol> objectMap;
+
+	public ContextFreeGrammar() {
+		objectMap = new HashMap<>();
+		objectMap.put("", ContextFreeEmptyWord.getInstance());
+	}
+	
+	// Returns the terminal symbol which corresponds to the given String.
+	// The String "" corresponds to the empty word.
+	public void createTerminalForString(String val) {
+		if (!objectMap.containsKey(val)) {
+			objectMap.put(val, new ContextFreeTerminal(val));
+		}
+	}
+
 }
--- src/main/parser/grammar/ContextFreeTerminal.java
+++ src/main/parser/grammar/ContextFreeTerminal.java
@@ -1,21 +1,12 @@
 package main.parser.grammar;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ContextFreeTerminal implements ContextFreeSymbol, ContextFreeTerminalSymbol {
 	
 	private String terminalValue;
 	
-	private static Map<String, ContextFreeTerminalSymbol> objectMap;
-	
-	static {
-		objectMap = new HashMap<>();
-		objectMap.put("", ContextFreeEmptyWord.getInstance());
-	}
-
 	@Override
 	public List<ContextFreeProduction> productionsForSymbol() {
 		List<ContextFreeProduction> returnList = new ArrayList<>();
@@ -25,17 +16,8 @@ public class ContextFreeTerminal implements ContextFreeSymbol, ContextFreeTermin
 		return returnList;
 	}
 	
-	private ContextFreeTerminal(String val) {
+    ContextFreeTerminal(String val) {
 		terminalValue = val;
-	}
-	
-	//Returns the terminal symbol which corresponds to the given String.
-	//The String "" corresponds to the empty word.
-	public static ContextFreeTerminalSymbol terminalForString(String val) {
-		if(!objectMap.containsKey(val)) {
-			objectMap.put(val, new ContextFreeTerminal(val));
-		}
-		return objectMap.get(val);
 	}
 	
 
